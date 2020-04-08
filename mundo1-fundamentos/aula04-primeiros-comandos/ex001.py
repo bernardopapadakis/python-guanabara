8bfff016be85032a9a939020bb6c2060a37bf4ab
Bernardo Papadakis <bernardo.papadakis@gmail.com>
2020-04-08 00:59:39 -0300

    exercícios aula04

---
+++ mundo1-fundamentos/aula04-primeiros-comandos/ex001.py
@@ -0,0 +1,8 @@
+"""
+Exercício 001
+
+Desafio: Crie um progama que escreva "Hello World" na tela.
+
+Resolução:
+"""
+print('Hello world!')
---
+++ mundo1-fundamentos/aula04-primeiros-comandos/ex002.py
@@ -0,0 +1,9 @@
+"""
+Exercício 002
+
+Desafio: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+
+Resolução:
+"""
+nome = input('Qual é o seu nome? ')
+print('É um prazer te conhecer, {}! '.format(nome))
