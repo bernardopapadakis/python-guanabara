94be94fbb27001533cef9ad0bfa581b766cbc0cb
Bernardo Papadakis <bernardo.papadakis@gmail.com>
2020-04-08 18:16:15 -0300

    música ex021

--- mundo1-fundamentos/aula08-ultilizando-modulos/ex021.py
+++ mundo1-fundamentos/aula08-ultilizando-modulos/ex021.py
@@ -6,4 +6,4 @@ Desafio: Faça um programa em python que abra e reproduza o áudio de um arquivo
 Resolução:
 """
 from playsound import playsound
-playsound('C:/pg/estudo/python/curso-em-video/mundo1-fundamentos/aula08/ex021.mp3')
+playsound('C:/pg/estudo/python/curso-em-video/mundo1-fundamentos/aula08-ultilizando-modulos/ex021.mp3')
